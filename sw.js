11cc66af5cfcaf3d41b1a1d3256ab6d1d96e419d
unknown <santo@fothergill.com>
2026-04-03 15:41:24 -0600

    files

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'checkin-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
